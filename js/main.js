db3e662da813707acdce618085920da3edbff3a8
SakaguchiMotonobu <sekenmunazanyo@gmail.com>
2020-06-23 23:22:57 +0900

    JavaScriptでビンゴシートを作ろう#08まで

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 "use strict";
 
 {
-  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  function createColumn(col) {
+    const source = [];
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
+    }
 
-  const b = [];
-  b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    const column = [];
+    for (let i = 0; i < 5; i++) {
+      column[i] = source.splice(
+        Math.floor(Math.random() * source.length),
+        1
+      )[0];
+    }
 
-  console.log(b);
+    return column;
+  }
+
+  const columns = [];
+  columns[0] = createColumn(0);
+  columns[1] = createColumn(1);
+  columns[2] = createColumn(2);
+  columns[3] = createColumn(3);
+  columns[4] = createColumn(4);
+  columns[2][2] = "FREE";
+
+  //   console.table(columns);
+
+  const bingo = [];
+  for (let row = 0; row < 5; row++) {
+    bingo[row] = [];
+    for (let col = 0; col < 5; col++) {
+      bingo[row][col] = columns[col][row];
+    }
+  }
+  console.table(bingo);
 }
